0fa1a7447b7f4ba1e820235fbe6d17dd817378b5
Bastien Metais <bastien.metais@ypsi.fr>
2021-05-19 11:22:43 +0000

    Work !

--- eventhandlers/cachet/mode/alert.pm
+++ eventhandlers/cachet/mode/alert.pm
@@ -26,6 +26,10 @@ use strict;
 use warnings;
 use centreon::plugins::http;
 use JSON::XS;
+use HTTP::Request();
+use Data::Dumper;
+
+# use JSON::MaybeXS qw(encode_json);
 
 sub new {
     my ($class, %options) = @_;
@@ -33,22 +37,17 @@ sub new {
     bless $self, $class;
 
     $options{options}->add_options(arguments => {
-        #
         'hostname:s'            => { name => 'hostname' },
-        'urlpath:s'             => { name => 'urlpath', default=>, default => 'https://localhost:8000/api/v1/components' },
+        'urlpath:s'             => { name => 'urlpath', default => '/' },
         'port:s'                => { name => 'port', default => 8000 },
         'proto:s'               => { name => 'proto', default => 'https' },
-        #
-        'api_key:s'             => { name => 'api_key'},
+        'api-key:s'             => { name => 'api_key'},
         'name:s'                => { name=> 'name' } ,
-		'message:s'             => { name=> 'message' ,} ,
-		#
+		'message:s'             => { name=> 'message'} ,
         'status:s'              => { name=> 'status' },
 		'visible:s'             => { name=> 'visible' },
-		#
-        'component_id:s'        => { name=> 'component_id' },
-		'component_status:s'    => { name=> 'component_status' },
-		'notify:s'              => { name=> 'notify' },
+        'component-id:s'        => { name=> 'component_id' },
+		'component-status:s'    => { name=> 'component_status' },
     });
 
     $self->{http} = centreon::plugins::http->new(%options);
@@ -58,7 +57,7 @@ sub new {
 sub check_options {
     my ($self, %options) = @_;
 
-    self->SUPER::init(%options);
+    $self->SUPER::init(%options);
     
     if (!defined($self->{option_results}->{hostname})) {
         $self->{output}->add_option_msg(short_msg => "You need to set --hostname option");
@@ -72,12 +71,48 @@ sub check_options {
         $self->{output}->add_option_msg(short_msg => "You need to set --component_id option");
         $self->{output}->option_exit();
     }
+    if (!defined($self->{option_results}->{component_status})) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --component_status option");
+        $self->{output}->option_exit();
+    }
+    if (($self->{option_results}->{component_status}) <1 && ($self->{option_results}->{component_status}) > 4) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --component_status between 1 and 4");
+        $self->{output}->option_exit();
+    }
     if (!defined($self->{option_results}->{status})) {
         $self->{output}->add_option_msg(short_msg => "You need to set --status option");
         $self->{output}->option_exit();
     }
+    if (($self->{option_results}->{status})<1 && ($self->{option_results}->{status})>4) {
+        $self->{output}->add_option_msg(short_msg => "You need to set --status between 1 and 4");
+        $self->{output}->option_exit();
+    }
     $self->{http}->set_options(%{$self->{option_results}});
 }
+
+sub run {
+    my ($self, %options) = @_;
+    
+    my $url = $self->{option_results}->{urlpath} . "api/v1/incidents";
+
+    $self->{http}->add_header(key => 'Content-Type', value => 'application/json');
+    $self->{http}->add_header(key => 'X-Cachet-Token', value => $self->{option_results}->{api_key});
+    print('ok');
+    my $data = { name => $self->{option_results}->{name}, message => $self->{option_results}->{message}, status => $self->{option_results}->{status}, visible => $self->{option_results}->{visible}, component_id => $self->{option_results}->{component_id}, component_status => $self->{option_results}->{component_status}};
+    my $encoded_data = encode_json($data);
+    print Dumper($encoded_data);
+    my $response = $self->{http}->request(url_path => $url,
+    method => 'POST', query_form_post => $encoded_data);
+    print Dumper($response)
+}
+
+1;
+
+
+
+
+
+
 
 
 
